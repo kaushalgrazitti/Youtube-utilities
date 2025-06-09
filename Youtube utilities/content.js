edbdb6c705d41625b69f8de40857a201eb79117c
Rahul kumar <Rahul.kumar1@grazitti.com>
2025-06-09 13:04:00 +0530

    updated content script to fix ad skip dislike count showing issue

--- Youtube utilities/content.js
+++ Youtube utilities/content.js
@@ -2,13 +2,15 @@ let videoId;
 let dislikeCount;
 let dislikeBtn;
 let prevLink;
+let handleNavigateIntervalId;
+let handleClickIntervalId;
 
-chrome.storage.local.set({adSkipperActive : false, showDislikesActive : false});
+chrome.storage.local.set({ adSkipperActive: false, showDislikesActive: false });
 
-function toggleSkipBehaviour (condition) {
-    if(condition)
+function toggleSkipBehaviour(condition) {
+    if (condition)
         window.addEventListener("keydown", skip);
-    else window.removeEventListener("keydown",skip);
+    else window.removeEventListener("keydown", skip);
 }
 
 function skip(e) {
@@ -16,57 +18,59 @@ function skip(e) {
     let video = document.querySelector(".ad-showing .html5-main-video");
 
     if (video && e.key === 'q') {
-        
-        const skipBtnList = document.querySelectorAll(".ytp-skip-ad-button, .ytp-ad-skip-button-modern");
-        
-        if(skipBtnList.length)
 
-            skipBtnList.forEach(e => e.click());
-            
-        if(video){
-            try{
+        const skipBtnList = document.querySelectorAll(".ytp-skip-ad-button, .ytp-ad-skip-button-modern");
+
+        if (skipBtnList.length)
+
+            skipBtnList.forEach(element => element.click());
+
+        if (video) {
+            try {
                 video.currentTime = video.duration;
             }
-            catch(e){}
+            catch (e) { }
         }
 
     }
 }
 
-function toggleDislikeBehaviour (condition) {
+function toggleDislikeBehaviour(condition) {
 
     prevLink = window.location.href;
-    
-    if(condition){
 
+    if (condition) {
         handleNavigate(1);
-        handleClick();
-        
-        navigation.addEventListener("navigate",navigationEventHandlerFunctionMain);
+        navigation.addEventListener("navigate", navigationEventHandlerFunctionMain);
     }
-    else{
+    else {
         let dislikeDiv = document.querySelector("#dislikeDiv");
-        if(dislikeDiv)
+        if (dislikeDiv)
             dislikeDiv.remove();
-        dislikeBtn = document.querySelector(".YtDislikeButtonViewModelHost .yt-spec-button-shape-next");
-        dislikeBtn.style.gap = "0px";
-        
-        navigation.removeEventListener("navigate",navigationEventHandlerFunctionMain);
-        document.removeEventListener("click",updateCount);
+        dislikeBtn = document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this']");
+
+        if (dislikeBtn)
+            dislikeBtn.style.gap = "0px";
+
+        clearInterval(handleNavigateIntervalId);
+        clearInterval(handleClickIntervalId);
+
+        navigation.removeEventListener("navigate", navigationEventHandlerFunctionMain);
+        document.removeEventListener("click", updateCount);
     }
 
 }
 
-function navigationEventHandlerFunctionMain(){
+function navigationEventHandlerFunctionMain() {
 
     let wasMiniplayerActive = document.querySelector("ytd-miniplayer").hasAttribute("active");
     // document.querySelector("ytd-miniplayer").active not working and giving undefined everytime even though working fine in browser.
-    
+
     setTimeout(() => {
         let currLink = window.location.href;
         if (prevLink != currLink) {
             if
-            (
+                (
                 (
                     wasMiniplayerActive &&
                     currLink.match(/[?&]v=([^&]+)/) &&
@@ -74,7 +78,7 @@ function navigationEventHandlerFunctionMain(){
                     document.querySelector("ytd-watch-flexy").getAttribute("video-id")
                 )
             )
-            handleNavigate(1);
+                handleNavigate(1);
             else handleNavigate();
             prevLink = currLink;
         }
@@ -86,93 +90,104 @@ async function handleNavigate() {
     if (videoId) {
         let URL = `https://returnyoutubedislikeapi.com/votes?videoId=${videoId[1]}`;
         dislikeCount = (await (await fetch(URL)).json()).dislikes;
-        dislikeCount+=arguments.length===2?1:0;
         if (arguments.length) {
-            let intervalId = setInterval(() => {
-                if (document.querySelector(".YtDislikeButtonViewModelHost .yt-spec-button-shape-next")) {
-                    dislikeBtn = document.querySelector(".YtDislikeButtonViewModelHost .yt-spec-button-shape-next");
-                    clearInterval(intervalId);
+            handleNavigateIntervalId = setInterval(() => {
+                if (document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this']")) {
+                    dislikeBtn = document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this']");
+                    clearInterval(handleNavigateIntervalId);
                     insertDislikes();
                 }
             }, 0);
         }
         else {
-            dislikeBtn = document.querySelector(".YtDislikeButtonViewModelHost .yt-spec-button-shape-next");
-            let intervalId = setInterval(() => {
-                if (dislikeBtn != document.querySelector(".YtDislikeButtonViewModelHost .yt-spec-button-shape-next")) {
-                    dislikeBtn = document.querySelector(".YtDislikeButtonViewModelHost .yt-spec-button-shape-next");
-                    clearInterval(intervalId);
+            dislikeBtn = document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this']");
+            handleNavigateIntervalId = setInterval(() => {
+                if (dislikeBtn != document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this']")) {
+                    dislikeBtn = document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this']");
+                    clearInterval(handleNavigateIntervalId);
                     insertDislikes();
                 }
             }, 0);
         }
     }
     videoId = window.location.href.match("(?<=shorts/)[^/?]+");
-    if(videoId){
+    if (videoId) {
         let URL = `https://returnyoutubedislikeapi.com/votes?videoId=${videoId[0]}`;
     }
 }
 
-function formatDislike() {
-                
+function formatDislikeBtn() {
     dislikeBtn.style.width = "auto";
     dislikeBtn.style.gap = "4px";
+}
 
-    if (dislikeCount > 999 && dislikeCount <= 9999)
-        dislikeCount = Math.floor(dislikeCount / 100) / 10 + "K";
-    
-    else if (dislikeCount > 9999 && dislikeCount <= 999999)
-        dislikeCount = Math.floor(dislikeCount / 1000) + "K";
-    
-    else if (dislikeCount > 999999 && dislikeCount <= 9999999)
-        dislikeCount = Math.floor(dislikeCount / 100000) / 10 + "M";
-    
-    else if (dislikeCount > 9999999)
-        dislikeCount = Math.floor(dislikeCount / 1000000) + "M";
-    
+function formatDislikeCount(count) {
+
+    if (count <= 999)
+        return count;
+
+    if (count > 999 && count <= 9999)
+        return Math.floor(count / 100) / 10 + "K";
+
+    if (count > 9999 && count <= 999999)
+        return Math.floor(count / 1000) + "K";
+
+    if (count > 999999 && count <= 9999999)
+        return Math.floor(count / 100000) / 10 + "M";
+
+    return Math.floor(count / 1000000) + "M";
+}
+
+function formatDislike(count) {
+    formatDislikeBtn();
+    return formatDislikeCount(count);
 }
 
 function insertDislikes() {
-
-    formatDislike();
-    
-    let dislikeDiv = document.querySelector("#dislikeDiv");
-    if (dislikeDiv) {
-        dislikeDiv.innerText = dislikeCount;
-    }
-    else {
+    if (document.querySelector("#dislikeDiv") == null) {
         dislikeDiv = document.createElement("div");
         dislikeDiv.id = "dislikeDiv";
-        dislikeDiv.innerText = dislikeCount;
         dislikeBtn.append(dislikeDiv);
+        handleUpdateCount();
     }
 }
 
-function handleClick(){
-    let intervalId = setInterval(()=>{
-        if(document.querySelector(".YtSegmentedLikeDislikeButtonViewModelSegmentedButtonsWrapper")){
-            clearInterval(intervalId);
-            document.addEventListener("click",updateCount());
+function handleUpdateCount() {
+    handleClickIntervalId = setInterval(() => {
+        if (
+            document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this'] .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill")
+            &&
+            document.querySelectorAll(".yt-spec-button-shape-next[title = 'I like this'] .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill, " + 
+                ".yt-spec-button-shape-next[title = 'Unlike'] .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill"
+            ).length > 0
+        ) {
+            clearInterval(handleClickIntervalId);
+            document.querySelector(".yt-spec-button-shape-next[title = 'I dislike this'] .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill")
+                .addEventListener("click", updateCount());
+            document.querySelectorAll(".yt-spec-button-shape-next[title = 'I like this'] .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill, " + 
+                ".yt-spec-button-shape-next[title = 'Unlike'] .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill"
+            )[0]?.addEventListener("click", updateCount);
         }
-    },0);
+    }, 0);
 }
 
-function updateCount(){
-    setTimeout(()=>{
-        if(document.querySelector(".YtDislikeButtonViewModelHost .yt-spec-button-shape-next").getAttribute("aria-pressed")==='true'){
-            handleNavigate(1,1);
+function updateCount() {
+    setTimeout(() =>{
+        const dislikeDiv = document.querySelector("#dislikeDiv");
+        if (dislikeDiv && dislikeBtn) {
+            dislikeDiv.innerText = formatDislike(dislikeCount + (dislikeBtn.getAttribute("aria-pressed") == 'true' ? 1 : 0));
         }
-        else handleNavigate(1);
-    },500);
+    }, 0);
+
     return updateCount;
 }
 
-chrome.storage.onChanged.addListener((changes, namespace)=>{
-    if(namespace === "local"){
-        if(changes.adSkipperActive)
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (namespace === "local") {
+        if (changes.adSkipperActive)
             toggleSkipBehaviour(changes.adSkipperActive.newValue);
-        
-        if(changes.showDislikesActive)
+
+        if (changes.showDislikesActive)
             toggleDislikeBehaviour(changes.showDislikesActive.newValue);
     }
 });
--- Youtube utilities/popup.js
+++ Youtube utilities/popup.js
@@ -1,45 +1,45 @@
-(async ()=>{
+(async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     let body = document.querySelector("body");
     let message = document.createElement("div");
     message.innerHTML = `Press <span>'Q'</span> to skip Ad.`;
     message.classList.add("messageStyling");
-    
+
     if (tab.url.match(/^https:\/\/www\.youtube\.com\//)) {
         let toggler = [...document.querySelectorAll(".toggle-switch")];
         toggler.forEach(btn => btn.addEventListener("click", e => {
             let slider = btn.querySelector("div");
             slider.classList.toggle("slide-toggler");
         }));
-    
+
         let [adSkipper, showDislikes] = toggler;
 
         chrome.storage.local.get("adSkipperActive", result => {
-            if(result.adSkipperActive){
+            if (result.adSkipperActive) {
                 adSkipper.querySelector("div").classList.add("slide-toggler");
                 body.appendChild(message);
             }
         });
         chrome.storage.local.get("showDislikesActive", result => {
-            if(result.showDislikesActive)
+            if (result.showDislikesActive)
                 showDislikes.querySelector("div").classList.add("slide-toggler");
         });
-    
+
         adSkipper.addEventListener("click", () => {
             chrome.storage.local.get("adSkipperActive", result => {
                 let adSkipperActive = !result.adSkipperActive;
-                chrome.storage.local.set({adSkipperActive : adSkipperActive});
-                if(adSkipperActive)
+                chrome.storage.local.set({ adSkipperActive: adSkipperActive });
+                if (adSkipperActive)
                     body.appendChild(message);
                 else
                     body.removeChild(message);
             });
         });
-    
+
         showDislikes.addEventListener("click", () => {
-            chrome.storage.local.get("showDislikesActive" , result => chrome.storage.local.set({showDislikesActive : !result.showDislikesActive}));
+            chrome.storage.local.get("showDislikesActive", result => chrome.storage.local.set({ showDislikesActive: !result.showDislikesActive }));
         });
-                
+
     }
     else {
         body.outerHTML = `
